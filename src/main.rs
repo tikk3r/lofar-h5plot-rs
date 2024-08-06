e9a0677a8eb936f33117bb9a66f3edf3c1765b77
Frits Sweijen <tikk3r@users.noreply.github.com>
2024-08-06 21:02:41 +0100

    Implement antenna iteration

--- src/main.rs
+++ src/main.rs
@@ -23,11 +23,12 @@ struct Args {
 }
 
 fn wrap_phase(p: f64) -> f64 {
-    let wrapped = (p + std::f64::consts::PI).rem_euclid(2.0 * std::f64::consts::PI) - std::f64::consts::PI;
+    let wrapped =
+        (p + std::f64::consts::PI).rem_euclid(2.0 * std::f64::consts::PI) - std::f64::consts::PI;
     wrapped
 }
 
-fn normalise_phase(p: f64) -> f64{
+fn normalise_phase(p: f64) -> f64 {
     //let positive = p + std::f64::consts::PI;
     let min = -std::f64::consts::PI;
     //let min = 0.0;
@@ -35,9 +36,13 @@ fn normalise_phase(p: f64) -> f64{
 }
 
 #[cached]
-fn get_data(h5parm: String, solset: String, soltab: String, idx_ant: i32) -> ArrayBase<OwnedRepr<f64>, Dim<[usize; 2]>>{
-    let h5 =
-        lofar_h5parm_rs::H5parm::open(&h5parm, false).expect("Failed to read h5parm");
+fn get_data(
+    h5parm: String,
+    solset: String,
+    soltab: String,
+    idx_ant: i32,
+) -> ArrayBase<OwnedRepr<f64>, Dim<[usize; 2]>> {
+    let h5 = lofar_h5parm_rs::H5parm::open(&h5parm, false).expect("Failed to read h5parm");
     let ss = h5.get_solset(solset).unwrap();
     let st = ss.get_soltab(soltab).unwrap();
     let data = st.get_values();
@@ -55,12 +60,23 @@ fn render_plot(
     height: i32,
 ) -> slint::Image {
     let aspect = width as f64 / height as f64;
-    let mut pixel_buffer = slint::SharedPixelBuffer::new(width as u32, (width as f64 / aspect) as u32);
+    let mut pixel_buffer =
+        slint::SharedPixelBuffer::new(width as u32, (width as f64 / aspect) as u32);
 
     // TODO: replace this with a data buffer to avoid reading the file every time
     //println!("Loading data from h5parm");
-    let data_ref = get_data(h5parm.to_string(), solset.to_string(), soltab.to_string(), idx_refant);
-    let data_ant = get_data(h5parm.to_string(), solset.to_string(), soltab.to_string(), idx_ant);
+    let data_ref = get_data(
+        h5parm.to_string(),
+        solset.to_string(),
+        soltab.to_string(),
+        idx_refant,
+    );
+    let data_ant = get_data(
+        h5parm.to_string(),
+        solset.to_string(),
+        soltab.to_string(),
+        idx_ant,
+    );
     let naxis1 = data_ant.shape()[0] as usize;
     let naxis2 = data_ant.shape()[1] as usize;
 
@@ -93,22 +109,21 @@ fn render_plot(
     //let ch: cube_helix::CubeHelix = Default::default();
     let color = colorgrad::sinebow();
 
-
     //println!("== Drawing pixels");
     chart
         .draw_series(
             (0..naxis1)
-                .flat_map(move |i| {
-                    (0..naxis2).map(move |j| (i, j))
-                })
+                .flat_map(move |i| (0..naxis2).map(move |j| (i, j)))
                 .map(|(i, j)| {
                     //let color = ch.get_color(normalise_phase(wrap_phase(d)));
                     let d = data_ant[[i, j]] - data_ref[[i, j]];
                     let c = color.at(normalise_phase(wrap_phase(d))).to_linear_rgba_u8();
                     Rectangle::new(
-                        [(i as i32, (naxis2 - j) as i32), ((i + 1) as i32, (naxis2 - j + 1) as i32)],
-                        RGBAColor(c.0, c.1, c.2, (c.3 as f64) / 255.)
-                        .filled(),
+                        [
+                            (i as i32, (naxis2 - j) as i32),
+                            ((i + 1) as i32, (naxis2 - j + 1) as i32),
+                        ],
+                        RGBAColor(c.0, c.1, c.2, (c.3 as f64) / 255.).filled(),
                     )
                 }),
         )
@@ -154,7 +169,7 @@ fn main() -> Result<(), slint::PlatformError> {
         .into_iter()
         .map(|x| slint::StandardListViewItem::from(x.as_str()))
         .collect();
-    let stations_model = std::rc::Rc::new(slint::VecModel::from(stations));
+    let stations_model = std::rc::Rc::new(slint::VecModel::from(stations.clone()));
 
     let refants: Vec<slint::SharedString> = ants
         .clone()
@@ -176,21 +191,42 @@ fn main() -> Result<(), slint::PlatformError> {
     ui.set_solset(ss_names[0].clone().into());
     ui.set_soltab(st_names[0].clone().into());
 
-    // TODO: cache should be created here
-
     ui.on_plot({
         let ui_handle = ui.as_weak();
         move || {
             let ui = ui_handle.unwrap();
             let antname = ui.get_current_antenna().text;
-            println!("Plotting {}", antname);
+            //println!("Plotting {}", antname);
             let ui2 = PlotWindow2D::new().expect("Failed to create plot window.");
+
             ui2.set_window_title(antname);
             ui2.set_idx_ant(ui.get_current_antenna_idx());
             ui2.set_idx_refant(ui.get_current_ref_antenna_idx());
             ui2.set_h5parm(h5name.clone().into());
             ui2.set_solset(ui.get_solset());
             ui2.set_soltab(ui.get_soltab());
+
+            ui2.on_decrease_ant({
+                let handle = ui2.as_weak().unwrap();
+                let stations2 = stations.clone();
+                move || {
+                    let new_idx = (handle.get_idx_ant() - 1) as usize;
+                    let sl = stations2[new_idx].text.clone();
+                    handle.set_idx_ant(new_idx as i32);
+                    handle.set_window_title(sl.clone());
+                }
+            });
+
+            ui2.on_increase_ant({
+                let handle = ui2.as_weak().unwrap();
+                let stations2 = stations.clone();
+                move || {
+                    let new_idx = (handle.get_idx_ant() + 1) as usize;
+                    let sl = stations2[new_idx].text.clone();
+                    handle.set_idx_ant(new_idx as i32);
+                    handle.set_window_title(sl.clone());
+                }
+            });
 
             // TODO: the render function should somehow have access to the cache
             // to avoid loading data every time
